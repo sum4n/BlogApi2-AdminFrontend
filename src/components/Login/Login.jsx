057f9584b62d02e3a86beaae91a6ef477bd8d827
Suman Das <sum4ndas@gmail.com>
2025-07-01 06:18:56 +0530

    Create login component

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(email, password);
+    fetch("http://localhost:3000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(res.message);
+        if (res.message === "Login successful") {
+          // store token in localStorage
+          localStorage.setItem("jwt-token", res.token);
+          alert(res.message);
+          setEmail("");
+          setPassword("");
+          navigate("/");
+        } else {
+          alert(res.message);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <p>Admin Login:</p>
+      <form onSubmit={handleSubmit}>
+        {/* <label htmlFor="email">Admin email: </label> */}
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
