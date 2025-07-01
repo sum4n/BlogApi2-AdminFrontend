63e79f391c706f17a065f37a92187f46db0d2ebd
Suman Das <sum4ndas@gmail.com>
2025-07-01 06:23:37 +0530

    Get user with token and handle logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <>Admin</>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt-token");
+    if (token) {
+      fetch("http://localhost:3000/api/user", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => setUser(data.user))
+        .catch((err) => {
+          console.log("Token fetch failed", err);
+          localStorage.removeItem("jwt-token");
+        });
+    }
+  }, []);
+
+  const handleLogout = () => {
+    setUser(null);
+    localStorage.removeItem("jwt-token");
+  };
+
+  return (
+    <>
+      <NavigationBar user={user} handleLogout={handleLogout} />
+    </>
+  );
 }
 
 export default App;
